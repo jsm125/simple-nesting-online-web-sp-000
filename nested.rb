5fdd496eab9e12806ad61f38ebf2becb0e5d86b3
Matt Im <matthew.seiim@gmail.com>
2020-05-04 15:21:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,6 +84,7 @@ def adding_matz
      }
 
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+
 programmer_hash
 
 end
@@ -108,6 +109,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
 programmer_hash[:alan_kay][:known_for] = alans_new_info
+
 programmer_hash
      
 end
@@ -129,4 +131,7 @@ def adding_to_dennis
         }
      }
 
+programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+
+programmer_hash
 end
