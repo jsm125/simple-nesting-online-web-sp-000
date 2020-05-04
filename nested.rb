4110ac6ce0bf2e0a4c6d1686cc0e4de36d84ea54
Matt Im <matthew.seiim@gmail.com>
2020-05-04 14:08:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = 0
+programmer_hash[:yukihiro_matsumoto] = nil
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
 
